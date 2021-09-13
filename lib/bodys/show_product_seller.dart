519ac47d55267bbe5770e71ca7d1b07595cafa2d
kanjiz <kanjizm@gmail.com>
2021-09-13 08:19:25 +0700

    44 end

--- lib/bodys/show_product_seller.dart
+++ lib/bodys/show_product_seller.dart
@@ -6,6 +6,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shoppingmall/models/product_model.dart';
 import 'package:shoppingmall/ultility/my_constant.dart';
 import 'package:shoppingmall/widgets/show_prograess.dart';
+import 'package:shoppingmall/widgets/show_title.dart';
 
 class ShowProductSeller extends StatefulWidget {
   const ShowProductSeller({Key? key}) : super(key: key);
@@ -16,8 +17,8 @@ class ShowProductSeller extends StatefulWidget {
 
 class _ShowProductSellerState extends State<ShowProductSeller> {
   bool load = true;
+  bool? haveData;
 
-  
   @override
   void initState() {
     super.initState();
@@ -30,24 +31,46 @@ class _ShowProductSellerState extends State<ShowProductSeller> {
 
     String apiGetProductWhereIdSeller =
         '${MyConstant.domain}/shoppingmall/getProductWhereidSeller.php?isAdd=true&idSeller=$id';
-    await Dio()
-        .get(apiGetProductWhereIdSeller)
-        .then((value) {
-          print('value ==> $value');
-          for (var item in json.decode(value.data)) {
-            ProductModel model = ProductModel.fromMap(item);
-            print('value ==> ${model.name}');
-            setState(() {
-              load = false;
-            });
-          }
-        });
+    await Dio().get(apiGetProductWhereIdSeller).then((value) {
+      print('value ==> $value');
+
+      if (value.toString() == 'null') {
+        // nodata
+        setState(() {
+            load = false;
+            haveData = true;
+          });
+      } else {
+        // have data
+        for (var item in json.decode(value.data)) {
+          ProductModel model = ProductModel.fromMap(item);
+          print('value ==> ${model.name}');
+
+          setState(() {
+            load = false;
+            haveData = true;
+          });
+        }
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: load ? ShowProgress() : Text('Load Finish'),
+      body: load
+          ? ShowProgress()
+          : haveData!
+              ? Text('Have Data')
+              : Center(
+                child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ShowTitle(title: 'No Product', textStyle: MyConstant().h1Style()),
+                    ShowTitle(title: 'Plase Add Product', textStyle: MyConstant().h2Style()),
+                  ],
+                ),
+              ),
+              
       floatingActionButton: FloatingActionButton(
         onPressed: () =>
             Navigator.pushNamed(context, MyConstant.routeAddProduct),
